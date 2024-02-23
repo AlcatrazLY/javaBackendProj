82a63fd6ecd581341ed8facfa56208e944220e24
AlcatrazLY <dixonramirez834@gmail.com>
2024-02-23 11:05:23 -0600

    initial commit

---
+++ src/test/java/com/javaBackend/demo/JavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaBackend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
